068553c5db3dec86699ef34af68614b3f1dfa3f6
Maria Antonia Rincon Z <maria.rincon@offerup.com>
2023-10-31 06:44:34 -0500

    Removing comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,3 @@
-// module.exports = {
-//   env: {
-//     browser: true,
-//     es2021: true,
-//     jest: true,
-//   },
-//   extends: [
-//     // 'eslint:recommended',
-//     // 'plugin:@typescript-eslint/recommended',
-//     // 'plugin:react/recommended',
-//     // 'plugin:prettier/recommended',
-//     // 'prettier',
-//     "prettier",
-//   ],
-//   overrides: [
-//     {
-//       env: {
-//         node: true,
-//       },
-//       files: [".eslintrc.{js,cjs}"],
-//       parserOptions: {
-//         sourceType: "script",
-//       },
-//     },
-//   ],
-//   parser: "@typescript-eslint/parser",
-//   parserOptions: {
-//     ecmaVersion: "latest",
-//     sourceType: "module",
-//   },
-//   plugins: ["@typescript-eslint", "react", "prettier"],
-//   rules: {
-//     "prettier/prettier": ["error"],
-//   },
-// };
-
 module.exports = {
   extends: ['react-app', 'prettier'],
   plugins: ['prettier'],
